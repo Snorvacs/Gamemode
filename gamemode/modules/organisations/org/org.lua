634b40600b677b62ce2ea6f4974752bdc8a7180c
InfernusNM <infernusdev@gmail.com>
2018-02-27 17:46:56 +1100

    Updated Org class to work with new RankingTree

--- gamemode/modules/organisations/org/org.lua
+++ gamemode/modules/organisations/org/org.lua
@@ -1,6 +1,6 @@
 local runtime, exports = ...
 
-local Rank = runtime.require("./rank").Rank
+local RankingTree = runtime.require("./rank").RankingTree
 
 local Org = runtime.oop.create("Org")
 exports["Org"] = Org
@@ -9,19 +9,5 @@ function Org:__ctor()
     -- Defaults
     self.color = Color(128, 128, 128)
     self.name = "Unnamed Organisation"
-
-    self.ranks = {}
+    self.rankingTree = RankingTree:__new()
 end
-
-function Org:addRank(position)
-    self.ranks[position] = Rank:__new()
-end
-
-function Org:removeRank(position)
-    self.ranks[position] = nil
-end
-
-function Org:getRanks()
-    return self.ranks
-end
-
